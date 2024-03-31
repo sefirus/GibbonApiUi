53e7782292cb63593f3e381d45daa1620524d323
sefirus <sashasam348@gmail.com>
2024-03-31 23:55:14 +0300

    Set up basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutModule } from './layout/layout.module';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutModule, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'https://localhost:7113/api',
+    authBaseUrl: 'https://localhost:7030/'
+  };
+  
