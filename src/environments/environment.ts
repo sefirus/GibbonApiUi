2901a0631de5ccfaf91c495db183bc437d823df0
sefirus <70850893+sefirus@users.noreply.github.com>
2024-05-24 23:47:50 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://gibbon-api-93a291d28d3c.herokuapp.com',
+    apiBaseUrl: 'https://gibbon-api-93a291d28d3c.herokuapp.com/api',
     authBaseUrl: 'https://gibbon-api-auth-2a2b01eb2f22.herokuapp.com'
   };
