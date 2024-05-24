414bae570e493175c663f60840f226dbffa13d6a
sefirus <70850893+sefirus@users.noreply.github.com>
2024-05-24 23:28:17 +0300

    Create environment.production.ts

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiBaseUrl: 'https://gibbon-api-93a291d28d3c.herokuapp.com',
+    authBaseUrl: 'https://gibbon-api-auth-2a2b01eb2f22.herokuapp.com'
+  };
